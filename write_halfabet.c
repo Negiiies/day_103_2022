f774a6824f9676c2b5b259e2ab1de9881dead503
LE Dylan <dylan.le@ecole-89.com>
2022-10-03 11:02:31 +0200

    halfabet

---
+++ write_halfabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+void stu_putchar(char c)
+{
+    write(1, &c, 1);
+}
+int main(void)
+{
+int i;
+i = 'a';
+while(i <= 'z')
+{
+    stu_putchar(i);
+    i = i + 2;
+ }
+ {
+     stu_putchar('\n');
+ }
+}
