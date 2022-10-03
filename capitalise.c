b7dfa16cd5497121441dbbc56e6c05c039ca3b55
LE Dylan <dylan.le@ecole-89.com>
2022-10-03 14:15:56 +0200

    capitalise

---
+++ capitalise.c
@@ -0,0 +1,11 @@
+char stu_capitalise(char letter)
+{
+    if ((letter >= 'a') && (letter<='z'))
+{
+    letter = letter -32;
+    return(letter);
+        }
+    else{
+        return(letter);
+    }
+}
---
+++ test_capitalise.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char stu_capitalise(char letter);
+int main(void)
+{
+    printf("%c\n", stu_capitalise('!'));
+    printf("%c\n", stu_capitalise('b'));
+ }
