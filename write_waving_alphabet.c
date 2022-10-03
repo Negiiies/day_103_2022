a24b7c6c246407bcf90123c2c60c70239b707299
LE Dylan <dylan.le@ecole-89.com>
2022-10-03 15:32:57 +0200

    waving_alphabet

---
+++ write_waving_alphabet.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+void stu_putchar(char c)
+{
+    write(1, &c, 1);
+}
+int main(void)
+{
+    int i;
+    i = 'A';
+    while (i !='[')
+        {
+            if (i < 92)
+                {
+        stu_putchar(i);
+        i = i + 33;
+                }else{
+                stu_putchar(i);
+                i = i-31;
+            }
+            
+}
+}
