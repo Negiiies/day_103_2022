c5ceb86f76db26828beaaee8693c243d6051ac6f
LE Dylan <dylan.le@ecole-89.com>
2022-10-10 09:35:59 +0200

    update 2

--- write_descending_digits.c
+++ write_descending_digits.c
@@ -1,19 +1,19 @@
 #include <unistd.h>
- #include <stdio.h>
- void stu_putchar(char c)
- {
-     write(1, &c, 1);
- }
- int main(void)
- {
-     int i;
-     i = '9';
-     while(i >= '0')
- {
-         stu_putchar(i);
-         i = i - 1;
+#include <stdio.h>
+void stu_putchar(char c)
+{
+    write(1, &c, 1);
 }
-     {
-     stu_putchar('\n');
- }
+int main(void)
+{
+    int i;
+    i = '9';
+    while(i >= '0')
+        {
+            stu_putchar(i);
+            i = i - 1;
+        }
+    {
+        stu_putchar('\n');
+    }
 }
